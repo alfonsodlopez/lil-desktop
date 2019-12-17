2916ef1585bac11085a191c06faa9323eeb067c0
Alfonso Lopez <alfonosdlopez@gmail.com>
2019-12-17 14:13:33 -0800

    Update readme.md

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # LiL-Desktop
 
+This is an [Electron](https://electronjs.org/) based app to create a native desktop experience with LinkedIn Learning.
+
+You will need nodeJS to use this run this application
+`https://docs.npmjs.com/downloading-and-installing-node-js-and-npm`
+
+**How to use**
+1. Clone the repository: `git clone https://github.com/alfonsodlopez/lil-desktop.git`
+2. Navigate to the directory: `cd ./lil-electron`
+3. Install the packages: `npm install`
+4. Start the application: `npm start`
 
 [CC0 1.0 (Public Domain)](LICENSE.md)
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Modules to control application life and create native browser window
 const { app, BrowserWindow, BrowserView } = require('electron')
 const path = require('path')
 
@@ -21,12 +20,10 @@ function createWindow() {
 
     let view = new BrowserView()
     mainWindow.setBrowserView(view)
-    //let bounds = mainWindow.getBounds()
-    //console.log(bounds)
     view.setBounds({ x: 0, y: 0, width: 800, height: 600 })
     view.webContents.loadURL('https://linkedin.com/learning')
 
-    // and load the index.html of the app.
+    // load the index.html of the app.
     mainWindow.loadFile('index.html')
 
     // Open the DevTools.
@@ -43,7 +40,6 @@ function createWindow() {
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
@@ -58,6 +54,3 @@ app.on('activate', function () {
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) createWindow()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
